309007cee5fb9123cc162f318953dc36ab53d372
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-08-19 15:48:58 -0500

    Completed MVP and one stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
 import {Route} from 'react-router-dom';
 import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 
 import SavedList from './Movies/SavedList';
 
-const App = () => {
-  // const [savedList, setSavedList] = useState( [] );
 
-  // const addToSavedList = movie => {
-  //   setSavedList( [...savedList, movie] );
-  // };
+const App = () => {
+  const [savedList, setSavedList] = useState( [] );
+
+  const addToSavedList = movie => {
+    setSavedList( [...savedList, movie] );
+  };
 
   return (
     <div>
       <SavedList list={savedList} />
       <Route exact path = "/" component = {MovieList} />
-      <Route exact path = {`/movies/${movie.id}`} />
+      <Route path = "/movies/:id" render={props => <Movie id={props.match.params.id} addToSavedList={addToSavedList} />}  />
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const MovieList = props => {
@@ -30,6 +31,7 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+    <Link to = {`/movies/${movie.id}`}>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -44,8 +46,10 @@ function MovieDetails({ movie }) {
         <div key={star} className="movie-star">
           {star}
         </div>
+    
       ))}
     </div>
+    </Link>
   );
 }
 
